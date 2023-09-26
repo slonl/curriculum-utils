c4c4029394c7f027814bbc6bd80ff40c20154a70
Auke van Slooten <auke@muze.nl>
2023-09-26 12:52:08 +0200

    added script to reset editor branches to latest version

---
+++ restore.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -o errexit  # Exit script when a command exits with non-zero status.
+set -o errtrace # Exit on error inside any functions or sub-shells.
+set -o nounset  # Exit script on use of an undefined variable.
+set -o pipefail # Return exit status of the last command in the pipe that exited with a non-zero exit code
+
+: readonly "${GIT:=git}"
+
+update() {
+    # get contexts from file
+    while read context; do
+    	echo "editor/${context}";
+    	"${GIT}" reset --hard origin/editor
+   	"${GIT}" -C "editor/${context}" checkout editor
+    done < curriculum-contexts.txt
+}
+
+if [[ "${BASH_SOURCE[0]}" != "$0" ]]; then
+  export -f update
+else
+  update "${@}"
+  exit $?
+fi
